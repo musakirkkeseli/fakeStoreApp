6a15c13636981f8b5998f5fd1f8ec9f9070a626f
Musa KIRKKESELİ <musakirkeseli@gmail.com>
2023-02-09 04:36:02 +0300

    Initial Commit

---
+++ FakeStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStore
+//
+//  Created by Musa KIRKKESELİ on 9.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
